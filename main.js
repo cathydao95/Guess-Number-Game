9d07baaaf6fb34d9e08a397069b488c4eab3bc85
Cathy Dao <cathydao95@gmail.com>
2022-04-07 11:20:32 -0700

    commit to hero

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ let guess = document.querySelector(".guess");
 let checkButton = document.querySelector(".check");
 let againButton = document.querySelector(".again");
 
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+
 let number = Math.trunc(Math.random() * 20 + 1);
 let score = 5;
 
@@ -12,27 +16,23 @@ let highScore = 0;
 checkButton.addEventListener("click", function () {
   let guessValue = Number(guess.value);
   if (!guessValue) {
-    document.querySelector(".message").textContent = "No Number!";
+    // document.querySelector(".message").textContent = "No Number!";
+    displayMessage("No Number!");
+    // When player Wins
   } else if (guessValue === number) {
     document.querySelector(".message").textContent = "You Win!";
     document.querySelector(".number").textContent = number;
     document.querySelector("body").style.backgroundColor = "green";
     if (score > highScore) {
-      document.querySelector(".high-score").textContent = score;
+      highScore = score;
+      document.querySelector(".high-score").textContent = highScore;
     }
-  } else if (guessValue > number) {
+    // If Guess Too High
+  } else if (guessValue !== number) {
     if (score > 1) {
-      document.querySelector(".message").textContent = "Too High";
+      document.querySelector(".message").textContent =
+        guessValue > number ? "Too high" : "Too Low";
       // Have to decrease the score BEFORE putting it in the textcontent
-      score--;
-      document.querySelector(".current-score").textContent = score;
-    } else {
-      document.querySelector(".message").textContent = "You Lose";
-      document.querySelector(".current-score").textContent = 0;
-    }
-  } else if (guessValue < number) {
-    if (score > 1) {
-      document.querySelector(".message").textContent = "Too Low";
       score--;
       document.querySelector(".current-score").textContent = score;
     } else {
@@ -50,6 +50,7 @@ againButton.addEventListener("click", function () {
   document.querySelector(".message").textContent = "Start Guessing...";
   document.querySelector(".current-score").textContent = score;
   document.querySelector(".number").textContent = "?";
-  document.querySelector(".guess").textContent = "";
+  document.querySelector(".guess").value = "";
   document.querySelector("body").style.backgroundColor = "black";
+  document.querySelector(".high-score").textContent = highScore;
 });
